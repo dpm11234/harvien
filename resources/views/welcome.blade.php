e32ca98195ca05842ad50043e70703418eafd3ab
dpm11234 <dpm11234@gmail.com>
2019-07-08 15:13:55 +0700

    structure folder project and sass

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,8 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/pages/navbar.css') }}" rel="stylesheet">
     <!-- Styles -->
     <style>
         html,

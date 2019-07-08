650e607d2132202f534ef66bffbbc677c9ac4ea1
dpm11234 <dpm11234@gmail.com>
2019-07-08 12:46:29 +0700

    lên lên

---
+++ database/migrations/2019_07_08_044734_products.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image')->nullable();
+            $table->timestamp('created')->nullable();
+            $table->timestamp('edited')->nullable();
+            $table->double('price')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}

5fa09e422f476e74984b493800e19a5a5072bd52
tuankhoi911 <17520648@gm.uit.edu.vn>
2019-07-09 19:48:13 +0700

    owl

--- resources/views/routes/home.blade.php
+++ resources/views/routes/home.blade.php
@@ -1,15 +1,34 @@
 @extends('welcome')
 @section('css')
 <link href="{{ asset('css/components/carousel.css') }}" rel="stylesheet">
-<link href="{{ asset('css/css/owl.carousel.css') }}" rel="stylesheet">
-<link href="{{ asset('css/css/owl.theme.default.min.css') }}" rel="stylesheet">
+<link href="{{ asset('owl/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css') }}" rel="stylesheet">
+<link href="{{ asset('owl/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css') }}" rel="stylesheet">
 @endsection
 
 
 @section('content')
 @include('components.carousel')
+@include('components.list-product')
 @endsection
 
 @push('script')
-<script src="{{ asset('js/owl/owl.carousel.js')}}"></script>
+<script src="{{ asset('owl/OwlCarousel2-2.3.4/dist/owl.carousel.min.js')}}"></script>
+<script>
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+</script>
 @endpush

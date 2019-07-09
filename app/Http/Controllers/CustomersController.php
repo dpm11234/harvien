4cbf727bed985407cea3442d7feae0cc24b97203
ca-global user.name captain <p-global user.email phuquy.uit@gmail.com>
2019-07-09 07:35:39 +0700

    update

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
+
 
 class CustomersController extends Controller
 {
     public function list()
     {
-        $customers = [
-            'John Doe',
-            'Jane Doe',
-            'Bob The Builder',
-        ];
+        $customers = Customer::all();
         return view('internals.customers', [
             'data_customers' => $customers,
         ]);
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'name'  => ['required', 'min:3'],
+            'email' => ['required', 'email'],
+        ]);
+
+        $customer = new Customer;
+        $customer->name     = request('name');
+        $customer->email    = request('email');
+        $customer->save();
+        return back();
     }
 }
--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -1,12 +1,28 @@
 @extends('layout')
 @section('content')
 
-    <h1>Customers</h1>
+<h1>Customers</h1>
+<form action="customers" method="POST" class="pb-5">
+    <div class="input-group">
+        <label for="name">Name</label> <input type="text" name="name">
+        @if ($nameError = $errors->first('name'))
+        <small class="alert alert-danger">{{$nameError}}</small>
+        @endif
+    </div>
+    <div class="input-group">
+        <label for="email">Email</label>
+        <input type="text" name="email">
+        @if ($emailError = $errors->first('email'))
+        <small class="alert alert-danger">{{$emailError}}</small>
+        @endif
+    </div>
+    @csrf
 
-    <ul>
-        @foreach ($data_customers as $customer)
-            <li>{{ $customer }}</li>
-        @endforeach
-    </ul>
+    <button type="submit">Add Customer</button>
+</form>
+<ul>
+    @foreach ($data_customers as $customer)
+    <li>{{ $customer->name }}</li>
+    @endforeach
+</ul>
 @endsection
-

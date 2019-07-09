f293d9a88e3a6113589b40ae1e5e7133c828ce84
ca-global user.name captain <p-global user.email phuquy.uit@gmail.com>
2019-07-09 14:43:09 +0700

    update javascript

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/layout/navbar.css') }}" rel="stylesheet">
-    <script
-  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
-  crossorigin="anonymous"></script>
+
     <!-- <script src="{{ asset('js/app.js')}}"></script> -->
     <!-- <link href="source/cover.css" rel="stylesheet"> -->
     <title>Harvee | Technology equitment</title>
@@ -33,8 +31,117 @@
     </div>
 
 
-  
+
       <script src="{{ asset('js/app.js') }}">
+      </script>
+      <script>
+       $(document).ready(() => {
+        console.log('start');
+
+        $("#my-cart").click(() => {
+            $("#cart-detail").fadeIn();
+            return false;
+        });
+        $("#cart-detail").click(() => {
+            return false;
+        });
+
+        // --------------------------------------
+
+        $("#my-cart-2").click(() => {
+            $("#cart-detail-2").fadeIn();
+            return false;
+        });
+        $("#cart-detail-2").click(() => {
+            return false;
+        });
+        $("body").click(({
+            target
+        }) => {
+
+            $("#cart-detail-2").fadeOut();
+        });
+
+        // --------------------------------------
+
+        $("#cate-1").click(() => {
+            $("#cate-detail-1").fadeIn();
+        });
+        $("#cate-detail-1").click(() => {
+            return false;
+        });
+        $("body").click(({
+            target
+        }) => {
+            if ($(target).is('#cate-1') || $(target).parents('#cate-1').length) return;
+            $("#cate-detail-1").fadeOut();
+        });
+
+        // --------------------------------------
+
+        $("#cate-2").click(() => {
+            $("#cate-detail-2").fadeIn();
+        });
+        $("#cate-detail-2").click(() => {
+            return false;
+        });
+        $("body").click(({
+            target
+        }) => {
+            if ($(target).is('#cate-2') || $(target).parents('#cate-2').length) return;
+            $("#cate-detail-2").fadeOut();
+        });
+
+        // --------------------------------------
+        $("#cate-scroll-1").click(() => {
+            $("#cate-detail-scroll-1").fadeIn();
+        });
+        $("#cate-detail-scroll-1").click(() => {
+            return false;
+        });
+        $("body").click(({
+            target
+        }) => {
+            if ($(target).is('#cate-scroll-1') || $(target).parents('#cate-scroll-1').length) return;
+            $("#cate-detail-scroll-1").fadeOut();
+        });
+
+        // --------------------------------------
+
+        $("#cate-scroll-2").click(() => {
+            $("#cate-detail-scroll-2").fadeIn();
+        });
+        $("#cate-detail-scroll-2").click(() => {
+            return false;
+        });
+        $("body").click(({
+            target
+        }) => {
+            if ($(target).is('#cate-scroll-2') || $(target).parents('#cate-scroll-2').length) return;
+            $("#cate-detail-scroll-2").fadeOut();
+        });
+
+
+        var nav = $("#nav-scroll");
+        var nonScroll = $('#non-scroll');
+        $(window).scroll(() => {
+                var distance = nonScroll.offset().top;
+            if ($(window).scrollTop() - distance >=0 ) {
+                if(!$("#nav-scroll").hasClass('nav-show'))
+                {
+                    $("#nav-scroll").addClass('nav-show');
+                    $('body').click();
+                }
+            } else {
+                if($("#nav-scroll").hasClass('nav-show'))
+                {
+                    $("#nav-scroll").removeClass('nav-show');
+                }
+            }
+
+        });
+    });
+      @yield('js')
     </script>
 </body>
 

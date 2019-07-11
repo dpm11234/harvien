131188020390d59c681c88e07efe674d832360e4
duong mau <dpm11234@gmail.com>
2019-07-11 10:46:21 +0700

    commit

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,76 +1,83 @@
-<div class="harvee-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-3">
-        <ul>
-          <li>
-            <h1>ADDRESS:</h1>
-            <p>HCM</p>
-          </li>
-          <li>
-              <h1>PHONE:</h1>
-              <p>XXXX <a href="#"></a> </p>
-          </li>
-          <li>
-              <h1>EMAIL:</h1>
-              <p><a href="">XXXX@XXX.XXX</a></p>
-          </li>
-          <li>
-              <h1>WORKING DAYS/HOURS:</h1>
-              <p>Mon - Sun / 9:00AM - 8:00PM</p>
-          </li>
-        </ul>
-      </div>
-      <div class="col-lg-2">
-        <h1>MY ACCOUNT</h1>
-        <ul>
-          <li>
-            <a href="#">About Us</a>
-          </li>
-          <li>
-            <a href="#">Contact Us</a>
-          </li>
-          <li>
-            <a href="#">My Account</a>
-          </li>
-          <li>
-            <a href="#">Orders History</a>
-          </li>
-          <li>
-            <a href="#">Advanced Search</a>
-          </li>
-          <li>
-            <a href="#">Login</a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-lg-3">
-        <h1>MAIN FEATURES</h1>
-        <ul>
-          <li>
-            <a href="#">Super Fast Magento Theme</a>
-          </li>
-          <li>
-            <a href="#">1st Fully working Ajax Theme</a>
-          </li>
-          <li>
-            <a href="#">20 Unique Homepage Layouts</a>
-          </li>
-          <li>
-            <a href="#">Mobile & Retina Optimized</a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-lg-4">
-        <h1>SUBSCRIBE NEWSLETTER</h1>
-        <p>Get all the latest information on Events,Sales and Offers. Sign up for newsletter today</p>
-        <div class="input-group mb-3">
-          <div class="input-group-prepend">
-            <button class="input-group-text" id="basic-addon1">SUBMIT</button>
-          </div>
-          <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+<div class="harvee-footer py-lg-5">
+    <div class="container">
+        <div class="row harvee-footer-top">
+            <div class="col-lg-3">
+                <div class="info">
+                    <ul>
+                        <li>
+                            <h1>ADDRESS:</h1>
+                            <p>HCM</p>
+                        </li>
+                        <li>
+                            <h1>PHONE:</h1>
+                            <p>XXXX <a href="#"></a> </p>
+                        </li>
+                        <li>
+                            <h1>EMAIL:</h1>
+                            <p><a href="">XXXX@XXX.XXX</a></p>
+                        </li>
+                        <li>
+                            <h1>WORKING DAYS/HOURS:</h1>
+                            <p>Mon - Sun / 9:00AM - 8:00PM</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <div class="widget">
+                    <h1>MY ACCOUNT</h1>
+                    <ul>
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                        <li>
+                            <a href="#">My Account</a>
+                        </li>
+                        <li>
+                            <a href="#">Orders History</a>
+                        </li>
+                        <li>
+                            <a href="#">Advanced Search</a>
+                        </li>
+                        <li>
+                            <a href="#">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="widget">
+                    <h1>MAIN FEATURES</h1>
+                    <ul>
+                        <li>
+                            <a href="#">Super Fast Magento Theme</a>
+                        </li>
+                        <li>
+                            <a href="#">1st Fully working Ajax Theme</a>
+                        </li>
+                        <li>
+                            <a href="#">20 Unique Homepage Layouts</a>
+                        </li>
+                        <li>
+                            <a href="#">Mobile & Retina Optimized</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <h1>SUBSCRIBE NEWSLETTER</h1>
+                <p>Get all the latest information on Events,Sales and Offers. Sign up for newsletter today</p>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username"
+                        aria-describedby="basic-addon1">
+                    <div class="input-group-prepend">
+                        <button class="input-group-text" id="basic-addon1">SUBMIT</button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>

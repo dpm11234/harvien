495f8578dbd5a0608d176e3aa5ef0a9a74fab2b4
ca-global user.name captain <p-global user.email phuquy.uit@gmail.com>
2019-07-12 15:56:45 +0700

    make auth

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('layouts.app')
 @section('content')
     <h1>Customers</h1>
     <ul>
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::view('about', 'about');
 
 
 Route::resource('customers', 'CustomersController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

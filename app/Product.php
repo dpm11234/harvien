7ce03f01ea58db748e601068d78b802e94243a8f
ca-global user.name captain <p-global user.email phuquy.uit@gmail.com>
2019-07-17 15:29:04 +0700

    add relationship

--- app/Product.php
+++ app/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     {
         return $this->belongsTo(Brand::class);
     }
+
+    public function orderDetails()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
 }

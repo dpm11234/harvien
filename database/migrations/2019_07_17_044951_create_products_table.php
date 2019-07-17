9455583024e0e565dbe408f5cc31aef737e6af0d
dpm11234 <dpm11234@gmail.com>
2019-07-17 14:50:55 +0700

    add migrate

---
+++ database/migrations/2019_07_17_044951_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('intro');
+            $table->string('thump')->default('');
+            $table->double('price');
+            $table->string('image-0');
+            $table->string('image-1');
+            $table->string('image-2');
+            $table->string('description'); // |
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- database/migrations/2019_07_17_074735_create_orders_table.php
+++ database/migrations/2019_07_17_074735_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Products extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class Products extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('image')->nullable();
-            $table->timestamp('created')->nullable();
-            $table->timestamp('edited')->nullable();
-            $table->double('price')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -29,6 +26,6 @@ class Products extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('orders');
     }
 }

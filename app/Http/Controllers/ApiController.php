3f9f44a463dc2a28459eb968055e8a9e82c5dd08
ca-global user.name captain <p-global user.email phuquy.uit@gmail.com>
2019-07-22 17:36:00 +0700

    add response method for unauthorized request

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Pagination\LengthAwarePaginator as Paginator;
-use Response;
 use \Illuminate\Http\Response as Res;
 
 /**
@@ -65,14 +64,31 @@ class ApiController extends Controller{
         ]);
         return $this->respondData($data, Res::HTTP_OK);
     }
+
+    /**
+     * @param string $message
+     * @return json response
+     */
     public function respondNotFound($message = 'Not Found!'){
         return $this->respond([
             'message' => $message,
         ], Res::HTTP_NOT_FOUND);
     }
+
+
+    /**
+     * @param string $message
+     * @return json response
+     */
     public function respondInternalError($message){
         return $this->respond(['message'=>$message], Res::HTTP_INTERNAL_SERVER_ERROR);
     }
+
+    /**
+     * @param string $message
+     * @param array $errors
+     * @return json response
+     */
     public function respondValidationError($message, $errors){
         $res = [
             'message' => $message,
@@ -92,20 +108,34 @@ class ApiController extends Controller{
         ], $data);
         return response($data, $status, $headers);
     }
+    /**
+     * @param mixed $data
+     * @param int $status
+     * @param array $headers
+     * @return json response
+     */
     public function respondData($data, int $status=Res::HTTP_OK, $headers = [])
     {
         return $this->respond(['data' => $data], $status, $headers);
     }
-    public function respondErrors($errors, int $status, $headers = []) {
-        return response(['errors'=> $errors], $status);
-    }
-    // public function respondWithError($message){
-    //     return $this->respond([
-    //         'status' => 'error',
-    //         'status_code' => Res::HTTP_UNAUTHORIZED,
-    //         'message' => $message,
-    //     ]);
 
-    //     return 
-    // }
+    // /**
+    //  * @param mixed $data
+    //  * @param int $status
+    //  * @param array $headers
+    //  * @return json response
+    //  */
+    // // public function respondErrors($errors, int $status, $headers = []) {
+    // //     return response(['errors'=> $errors], $status);
+    // // }
+
+    /**
+     * @param string $message
+     * @return json response
+     */
+    public function respondUnauthorized($message = 'Unauthorized Request!'){
+        return $this->respond([
+            'message' => $message,
+        ], Res::HTTP_UNAUTHORIZED);
+    }
 }
